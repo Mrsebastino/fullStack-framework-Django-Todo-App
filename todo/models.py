306f92f40b2e477b0b344ccf7551da217ab5f1a9
Sebastien Denommee <mrsebastino@gmail.com>
2020-06-17 09:50:59 +0000

    made Django form template to render in html

--- todo/models.py
+++ todo/models.py
@@ -7,5 +7,6 @@ class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False, default=False)
 
+    # to be able to see properly what we called our item
     def __str__(self):
         return self.name
