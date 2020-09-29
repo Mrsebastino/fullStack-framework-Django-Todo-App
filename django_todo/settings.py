a8846692dcf8d25fa490bd6f1e8ebe8d03e0d0a8
Sebastien Denommee <mrsebastino@gmail.com>
2020-09-29 11:59:21 +0000

    editing

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import dj_database_url
+
 
 # this mean if there an environment variable called DEVELOPMENT
 # it will be set to its value otherwise it will be false
